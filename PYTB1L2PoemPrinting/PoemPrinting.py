f03761f8974a59bbbd31e5abe6e382a61498c14d
Jaymar Westerlow <jaywesterlow71@gmail.com>
2020-09-14 23:17:14 +0200

    Added Poem Printing

---
+++ PYTB1L2PoemPrinting/PoemPrinting.py
@@ -0,0 +1,30 @@
+import time
+time.sleep(1)
+print("Please type 'verse 1' or 'verse 2'")
+vers1 = "Ik ben jaymar en ik woon in het bos"
+vers2 = "ik heb veel swag maar me tanden zitten los"
+
+
+
+antwoord_1 = input()
+if antwoord_1.lower() == "verse 1":
+  time.sleep(1)
+  print(vers1)
+elif antwoord_1.lower() == "verse 2":
+  time.sleep(1)
+  print(vers2)
+else:
+  print("Please type 'verse 1' or 'verse 2'")
+time.sleep(1)
+print("Please print the other verse")
+
+antwoord_1 = input()
+if antwoord_1.lower() == "verse 1":
+  time.sleep(1)
+  print(vers1)
+elif antwoord_1.lower() == "verse 2":
+  time.sleep(1)
+  print(vers2)
+else:
+  print("Please type 'verse 1' or 'verse 2'")
+time.sleep(1)
