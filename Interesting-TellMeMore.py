7bbf7e01a441ecec7a97a56f1d068b775a26edb3
Jaymar Westerlow <jaywesterlow71@gmail.com>
2020-09-26 20:01:27 +0200

    'Added Interesting, tell me more'

---
+++ Interesting-TellMeMore.py
@@ -0,0 +1,58 @@
+import time
+
+print('Hello You! ik ben Jaymar.')
+time.sleep(1)
+
+print("Wie ben jij?")
+name1 = input(">")
+time.sleep(1)
+
+print("Hallo " + name1 + ", hoe oud ben je?")
+age1 = input(">")
+time.sleep(1)
+
+print("Oké, waar woon je " + name1 + "?")
+home1 = input(">")
+time.sleep(1)
+
+print("Oké " + name1 + ", je bent dus " + age1 + " jaar oud en woont in " + home1 + "!")
+time.sleep(5)
+
+
+
+
+print("Hello you! Ik heb net " + name1 + " ontmoet, hoe heet jij?")
+name2 = input(">")
+time.sleep(1)
+
+print("Hallo " + name2 + ", hoe oud ben je?")
+age2 = input(">")
+time.sleep(1)
+
+print("Oké, waar woon je " + name2 + "?")
+home2 = input(">")
+time.sleep(1)
+
+print("Oké " + name2 + ", je bent dus " + age2 + " jaar oud en woont in " + home2 + "!")
+time.sleep(5)
+
+
+
+
+print("Hello you! Ik heb zojuist " + name1 + " en " + name2 + " ontmoet, mag ik vragen wie jij bent?")
+name3 = input(">")
+time.sleep(1)
+
+print("Hallo " + name3 + ", hoe oud ben je?")
+age3 = input(">")
+time.sleep(1)
+
+print("Oké, waar woon je " + name3 + "?")
+home3 = input(">")
+time.sleep(1)
+
+print("Oké " + name3 + ", je bent dus " + age3 + " jaar oud en woont in " + home3 + "!")
+time.sleep(3)
+
+
+print("Dus als ik het goed heb zijn jullie: " + name1 + ", " + name2 + " en " + name3 + ", zijn jullie; " + age1 + ", " + age2 + " en " + age3 + " en wonen jullie in; " + home1 + ", " + home2 + " en " + home3 + "!")
