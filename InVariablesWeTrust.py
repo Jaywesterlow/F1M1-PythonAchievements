b519d224757307dded80908c222d536a35116c8a
Jaymar Westerlow <jaywesterlow71@gmail.com>
2020-09-28 10:51:17 +0200

    Added In Variables We Trust

---
+++ InVariablesWeTrust.py
@@ -0,0 +1,21 @@
+char1 = "A"
+
+float1 = 3.5
+
+bool1 = True
+bool2 = bool1
+
+int1 = 3
+int2 = 6
+int3 = int1 + int2
+
+str1 = "abra"
+str2 = "kadabra"
+str3 = str1 + " " + str2
+
+
+print(char1)
+print(float1)
+print(bool2)
+print(int3)
+print(str3)
