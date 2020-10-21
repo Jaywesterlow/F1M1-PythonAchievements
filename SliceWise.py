8430949d84b5e07dff65eb99885092f0ceab7689
Jaymar Westerlow <jaywesterlow71@gmail.com>
2020-10-21 12:00:07 +0200

    Added Slice Wise& C:/Users/jaywe/AppData/Local/Programs/Python/Python38-32/python.exe c:/Users/jaywe/OneDrive/Desktop/School/F1M1PYT/pythonfolder1/SliceWise.py

---
+++ SliceWise.py
@@ -0,0 +1,15 @@
+tekst = 'De Python lessen worden gegeven voor Erik, Erwin en ook door Hidde'
+names = [tekst[37:41], tekst[43:49], tekst[61:66]]
+print(names)
+
+tekst = 'SD vakken zijn Python, UXD, Frontend development en Backend development en nog veel meer ...'
+vakken = [tekst[15:21], tekst[23:26], tekst[28:48], tekst[52:71]]
+print(vakken)
+
+tekst = 'Wat is hier het laatste woord?'
+laatsteZes = [tekst[22:23],tekst[24:29]]
+print(laatsteZes)
+
+tekst = 'Het www is ontwikkeld vanaf 1989 door Tim Berners-Lee'
+tweeStukjes = [tekst[5:7],tekst[8:10],tekst[39:41],tekst[42:44]]
+print(tweeStukjes)
